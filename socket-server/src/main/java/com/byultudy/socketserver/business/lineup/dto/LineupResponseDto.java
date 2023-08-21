fed3e51625b174432fa6917b305cc1ce2a7bc7fd
Jongmin Yoon <jongmin4943@gmail.com>
2023-08-22 00:22:38 +0900

    fix(server): LineupResponseDto createAt JsonFormat 추가

--- socket-server/src/main/java/com/byultudy/socketserver/business/lineup/dto/LineupResponseDto.java
+++ socket-server/src/main/java/com/byultudy/socketserver/business/lineup/dto/LineupResponseDto.java
@@ -1,9 +1,12 @@
 package com.byultudy.socketserver.business.lineup.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
 public record LineupResponseDto(
         String id,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime createdAt
 ) {
 
