a6bbd6e132e7896089271cd2e2a2eef52832f1da
jiyeong-dt <imjee02@gmail.com>
2023-08-24 00:32:29 +0900

    feat(server): redis, scheduler 환경 설정

--- socket-server/src/main/java/com/byultudy/socketserver/infra/config/RedisConfig.java
+++ socket-server/src/main/java/com/byultudy/socketserver/infra/config/RedisConfig.java
@@ -1,11 +1,15 @@
 package com.byultudy.socketserver.infra.config;
 
+import com.byultudy.socketserver.redis.service.RedisSubService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -32,5 +36,26 @@ public class RedisConfig {
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
         return redisTemplate;
+    }
+
+    //리스너어댑터 설정
+    @Bean
+    public MessageListenerAdapter messageListenerAdapter(RedisSubService redisSubService) {
+        return new MessageListenerAdapter(redisSubService);
+    }
+
+    //컨테이너 설정
+    @Bean
+    public RedisMessageListenerContainer redisContainer(MessageListenerAdapter messageListenerAdapter) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory());
+        container.addMessageListener(messageListenerAdapter, topic());
+        return container;
+    }
+
+    //pub/sub 토픽 설정
+    @Bean
+    public ChannelTopic topic() {
+        return new ChannelTopic("topic1");
     }
 }
