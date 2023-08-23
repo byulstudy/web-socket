74051b65a729e3f6811686bbb39c9cf0ed142200
jiyeong-dt <imjee02@gmail.com>
2023-08-24 00:31:21 +0900

    feat(client): socket 연습...

--- socket-client/components/TicketReservation.vue
+++ socket-client/components/TicketReservation.vue
@@ -1,5 +1,14 @@
 <template>
-	<common-button @click="$router.push('/lineup')">예약하기</common-button>
+	<common-button @click="bookTicket">예약하기</common-button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const bookTicket = async () => {
+	const now = $dayjs();
+	const { data } = await useAsyncData(
+		() => $fetch("/api/lineup", { method: "post", body: { at: now } }),
+		{},
+	);
+	await useRouter().push("/lineup");
+};
+</script>
